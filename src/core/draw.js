1faa4df4c2d165ac20bb56e5ca135415fde6a532
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-02-14 17:19:29 -0300

    refactor: Separa lógica de negócio em funções puras

---
+++ src/core/draw.js
@@ -0,0 +1,62 @@
+import { MAX_TENTATIVAS_SORTEIO, MENSAGENS } from "../constants/index.js";
+
+function gerarParAmigos(quemEntrega, possiveisRecebedores) {
+  const indexReceber = Math.floor(Math.random() * possiveisRecebedores.length);
+  const quemRecebe = possiveisRecebedores[indexReceber];
+
+  return {
+    quemEntrega,
+    quemRecebe,
+    possiveisRecebedoresRestantes: possiveisRecebedores.filter(
+      (amigo) => amigo !== quemRecebe
+    ),
+  };
+}
+
+export function gerarSorteio(amigos) {
+  let tentativas = 0;
+
+  while (tentativas < MAX_TENTATIVAS_SORTEIO) {
+    const resultado = amigos.reduce(
+      (acc, quemEntrega) => {
+        if (acc.falha) return acc;
+        const possiveisRecebedores = amigos.filter(
+          (amigo) => !acc.jaReceberam.includes(amigo) && amigo !== quemEntrega
+        );
+
+        if (possiveisRecebedores.length === 0) {
+          return { ...acc, falha: true };
+        }
+
+        const par = gerarParAmigos(quemEntrega, possiveisRecebedores);
+
+        return {
+          pares: [
+            ...acc.pares,
+            {
+              quemEntrega: par.quemEntrega,
+              quemRecebe: par.quemRecebe,
+            },
+          ],
+          jaReceberam: [...acc.jaReceberam, par.quemRecebe],
+          falha: false,
+        };
+      },
+      { pares: [], jaReceberam: [], falha: false }
+    );
+
+    if (!resultado.falha) {
+      return {
+        sucesso: true,
+        sorteio: resultado.pares,
+      };
+    }
+
+    tentativas++;
+  }
+
+  return {
+    sucesso: false,
+    erro: MENSAGENS.ERRO_SORTEIO,
+  };
+}
---
+++ src/core/validators.js
@@ -0,0 +1,46 @@
+import { MINIMO_PARTICIPANTES, MENSAGENS } from "../constants/index.js";
+
+export function validarNovoAmigo(nome, amigosAtuais) {
+  const nomeProcessado = nome.trim();
+
+  if (nomeProcessado === "") {
+    return {
+      valido: false,
+      erro: MENSAGENS.ERRO_CAMPO_VAZIO,
+    };
+  }
+
+  if (amigosAtuais.includes(nomeProcessado)) {
+    return {
+      valido: false,
+      erro: MENSAGENS.ERRO_NOME_DUPLICADO,
+    };
+  }
+
+  return {
+    valido: true,
+    nome: nomeProcessado,
+    erro: null,
+  };
+}
+
+export function validarSorteio(amigos) {
+  if (amigos.length < MINIMO_PARTICIPANTES) {
+    return {
+      valido: false,
+      erro: MENSAGENS.ERRO_MINIMO_PARTICIPANTES(amigos.length),
+    };
+  }
+
+  if (amigos.length % 2 !== 0) {
+    return {
+      valido: false,
+      erro: MENSAGENS.ERRO_QUANTIDADE_IMPAR,
+    };
+  }
+
+  return {
+    valido: true,
+    erro: null,
+  };
+}
